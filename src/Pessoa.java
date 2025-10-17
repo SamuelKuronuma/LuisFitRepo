96ff9482b23ee98fa45471b371435dc0fbb97a65
SamuelKuronuma <samuel.kuronuma@etec.sp.gov.br>
2025-10-17 10:14:44 -0300

    Adicionados atributos, construtores, getters e setters na classe pessoa

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,4 +1,47 @@
 
 public class Pessoa {
-
+	
+	//Atributo
+	
+	int idAt;
+	String nomeAt;
+	int idadeAt;
+	
+	//Construtores
+	
+	public Pessoa(int idPar, String nomePar, int idadePar){
+		
+		this.idAt = idPar;
+		this.nomeAt = nomePar;
+		this.idadeAt = idadePar;
+	
+	}
+	
+	//Getters
+	
+	public int PegarID(){
+		return idAt;
+	}
+	
+	public String PegarNome(){
+		return nomeAt;
+	}
+	
+	public int Pegaridade(){
+		return idadeAt;
+	}
+	
+	//Setters
+	
+	public void alterarID(int idPar){
+		this.idAt = idPar;
+	}
+	
+	public void alterarNome(String nomePar){
+		this.nomeAt = nomePar;
+	}
+	
+	public void alterarIdade(int idadePar){
+		this.idadeAt = idadePar;
+	}
 }
